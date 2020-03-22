18e8233ca2e8ec2bfb03efb20d2cc9adbed2bc21
monsieurbadia <contact@monsieurbadia.com>
2020-03-22 17:22:23 +0100

    W2GL: Deleted unused modules

--- src/camera/camera.perspective.js
+++
@@ -1,8 +0,0 @@
-import { reducerLoopObject } from '../reducer/reducer.object';
-
-export const PerspectiveCamera = options => options;
-
-export const createCamera = options => ( {
-  ...options,
-  camera: reducerLoopObject( options.camera, ( option ) => PerspectiveCamera( option ) )
-} );
