3b70e8c4a82b13c87aaa9b339150f527ece126b6
monsieurbadia <contact@monsieurbadia.com>
2020-03-23 00:59:47 +0100

    W2GL: Added Screen

---
+++ src/core/core.screen.js
@@ -0,0 +1,19 @@
+export class Screen {
+
+  constructor () {
+
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+
+    this.resize = this.resize.bind( this );
+
+  }
+
+  resize ( event ) {
+  
+    this.width = event.target.innerWidth;
+    this.height = event.target.innerHeight;
+  
+  }
+
+}
